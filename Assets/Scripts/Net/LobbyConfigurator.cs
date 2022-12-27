c789945e4f95cef5b9dd7a5efef1cde1f52f4ee7
ErymenkoSergey <eryomenkosergey10@gmail.com>
2022-12-27 10:07:41 +0700

    Adde new ui for Start scene.

---
+++ Assets/Scripts/Net/LobbyConfigurator.cs
@@ -0,0 +1,46 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyConfigurator : MonoBehaviour
+{
+    [SerializeField] private NetworkManager _networkManager;
+
+    [SerializeField] private Button _playGame;
+    [SerializeField] private Button _createMapGame;
+    [SerializeField] private Button _findGame;
+
+    [SerializeField] private GameObject _findGamePanel;
+    [SerializeField] private TMP_InputField _addressServer;
+    [SerializeField] private Button _connectToServer;
+
+    private void OnEnable()
+    {
+        _playGame.onClick.AddListener(() => _networkManager.StartClient()); // Client
+        _createMapGame.onClick.AddListener(() => _networkManager.StartHost()); //Host (Server + Client)
+        _findGame.onClick.AddListener(() => OpenFindPanel(true));
+        _addressServer.onEndEdit.AddListener(SetServerAddress);
+        _connectToServer.onClick.AddListener(() => OpenFindPanel(false)); 
+    }
+
+    private void OnDisable()
+    {
+        _playGame.onClick.RemoveAllListeners();
+        _createMapGame.onClick.RemoveAllListeners();
+        _findGame.onClick.RemoveAllListeners();
+        _addressServer.onEndEdit.RemoveAllListeners();
+        _connectToServer.onClick.RemoveAllListeners();
+    }
+
+    private void OpenFindPanel(bool isOpen)
+    {
+        _findGamePanel.SetActive(isOpen);
+    }
+
+    private void SetServerAddress(string address)
+    {
+        _networkManager.networkAddress = address;
+        _networkManager.StartClient();
+    }
+}
